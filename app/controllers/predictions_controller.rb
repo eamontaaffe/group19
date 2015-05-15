c35d8f6f59b1726c1b49981839d34b50eb299c1a
Eamon Taaffe <eamontaaffe@gmail.com>
2015-05-15 17:23:34 +1000

    need to alter the Interpolater module to ouput an id for postcodes

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,2 +1,11 @@
 class PredictionsController < ApplicationController
+  def get_prediction_by_postcode
+    params[:post_code]
+    params[:period]
+    @out = {}
+    respond_to do |format|
+      format.html
+      format.json { render json: JSON.pretty_generate(@out.as_json) }
+    end
+  end
 end
