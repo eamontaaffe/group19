e5d62022192616d02bf652d689e18b00452b28dc
Eamon Taaffe <eamontaaffe@gmail.com>
2015-05-15 15:58:23 +1000

    conversion to postcode done

--- lib/interpolator.rb
+++ lib/interpolator.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'json'
 require 'open-uri'
+require 'debugger'
 
 module Interpolator
   @@GEOCODE_API_KEY = "AIzaSyAc_WiZgYEUu9dGCiNcR_0v652lY2uugJ0"
@@ -17,5 +18,13 @@ module Interpolator
     geocode = JSON.parse(open(
       "#{@@GEOCODEURL}json?address=#{address}&key=#{@@GEOCODE_API_KEY}").read)
     return geocode["results"].first["geometry"]["location"]
+  end
+  def station_to_postcode(station)
+    address = "#{station.to_s.gsub(/\s/,'+')}+Victoria+Australia"
+    geocode = JSON.parse(open(
+      "#{@@GEOCODEURL}json?address=#{address}&key=#{@@GEOCODE_API_KEY}").read)
+    address_components = geocode["results"].first["address_components"]
+    postal_codes = address_components.select {|c| c["types"] == ["postal_code"]}
+    postal_codes.first["long_name"].to_i
   end
 end
