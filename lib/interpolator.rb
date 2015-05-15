ecb5765b62c090ad83832b60c1cb9cadd89f3a07
Eamon Taaffe <eamontaaffe@gmail.com>
2015-05-15 15:34:27 +1000

    station_to_postcode function added to Interpolator module

--- lib/interpolator.rb
+++ lib/interpolator.rb
@@ -1,9 +1,22 @@
 require 'nokogiri'
 require 'json'
+require 'open-uri'
+require 'debugger'
 
 module Interpolator
-  def postcode_to_lat_lon(postcode)
+  @@GEOCODE_API_KEY = "AIzaSyAc_WiZgYEUu9dGCiNcR_0v652lY2uugJ0"
+  @@GEOCODEURL = "https://maps.googleapis.com/maps/api/geocode/"
 
-    {lat: 0.0, lon: 0.0}
+  def postcode_to_lat_lon(postcode)
+    query_to_lat_lon(postcode)
+  end
+  def station_to_lat_lon(station)
+    query_to_lat_lon(station)
+  end
+  def query_to_lat_lon(query)
+    address = "#{query.to_s.gsub(/\s/,'+')}+Victoria+Australia"
+    geocode = JSON.parse(open(
+      "#{@@GEOCODEURL}json?address=#{address}&key=#{@@GEOCODE_API_KEY}").read)
+    return geocode["results"].first["geometry"]["location"]
   end
 end
