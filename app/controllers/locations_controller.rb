f66a9f7b0a3309c008a019334e8244f933501fda
Eamon Taaffe <eamontaaffe@gmail.com>
2015-05-15 17:01:39 +1000

    Locations controller is done, the default render is a json response and html can be requested with the .html extension. no view yet

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,24 @@
+require 'json'
+require 'date'
+
 class LocationsController < ApplicationController
+  def get_locations
+    @locations = Location.all
+
+    @out = {date: Date.today,
+      locations: []}
+    @locations.each do |loc|
+      @out[:locations] << {
+        id: loc.station,
+        lat: loc.lat,
+        lon: loc.lon,
+        last_update: loc.updated_at.strftime("%I:%M%P %F")
+        #"13:12pm	21-03-2015"
+      }
+    end
+    respond_to do |format|
+      format.html
+      format.json { render json: JSON.pretty_generate(@out.as_json) }
+    end
+  end
 end
