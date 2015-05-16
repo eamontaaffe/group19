22cab78eb297a322bf42857b2f0bd9443164b646
Eamon Taaffe <eamontaaffe@gmail.com>
2015-05-16 13:55:12 +1000

    created Interpolator sub-module Geocode moved all the geocode related methods into that. Added a model method to Location which returns neighbours with an radius of a location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,33 @@
+include Math
+
 class Location < ActiveRecord::Base
+
+  def self.find_neighbours(loc1, rad)
+    # loc1 = [lat,lon]
+    # radius is in meters
+    neighbours = Location.all.select do |loc2|
+      distance_to_location(loc1,[loc2.lat,loc2.lon]) < rad
+    end
+    return neighbours
+  end
+
+  private
+  def self.distance_to_location(loc1,loc2)
+    ## From http://stackoverflow.com/questions/12966638/how-to-calculate-the-
+    ## distance-between-two-gps-coordinates-without-using-google-m
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    # rm = rkm * 1000             # Radius in meters
+
+    dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad)* Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    rkm * c # Delta in meters
+  end
 end
--- lib/interpolator.rb
+++ lib/interpolator.rb
@@ -2,23 +2,35 @@ require 'nokogiri'
 require 'json'
 require 'open-uri'
 require 'debugger'
+include Math
 
 module Interpolator
+
+end
+
+module Geocode
   @@GEOCODE_API_KEY = "AIzaSyAc_WiZgYEUu9dGCiNcR_0v652lY2uugJ0"
   @@GEOCODEURL = "https://maps.googleapis.com/maps/api/geocode/"
 
   def postcode_to_lat_lon(postcode)
-    query_to_lat_lon(postcode)
+    geocode = query_to_json(postcode)
+    top_result = geocode["results"].first
+    debugger
+    out = top_result["geometry"]["location"]
+    id_info = top_result["address_components"].select{|c| c["types"].include? "colloquial_area"}
+    id = id_info.first["short_name"]
+    {lat: out["lat"], lon: out["lng"], id: id}
   end
   def station_to_lat_lon(station)
-    query_to_lat_lon(station)
+    geocode = query_to_json(station)
+    out = geocode["results"].first["geometry"]["location"]
+    {lat: out["lat"], lon: out["lng"]}
   end
-  def query_to_lat_lon(query)
+  def query_to_json(query)
     address = "#{query.to_s.gsub(/\s/,'+')}+Victoria+Australia"
     geocode = JSON.parse(open(
       "#{@@GEOCODEURL}json?address=#{address}&key=#{@@GEOCODE_API_KEY}").read)
-    out = geocode["results"].first["geometry"]["location"]
-    {lat: out["lat"], lon: out["lng"]}
+
   end
   def station_to_postcode(station)
     address = "#{station.to_s.gsub(/\s/,'+')}+Victoria+Australia"
