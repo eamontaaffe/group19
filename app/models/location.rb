853e368d972cb3653e203c6ceaac55cc7c633a8e
Eamon Taaffe <eamontaaffe@gmail.com>
2015-05-16 14:24:43 +1000

    inverse_distance_weighting is almost coded, it just needs to be tested once a few predictions have been made

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,13 +11,17 @@ class Location < ActiveRecord::Base
     return neighbours
   end
 
+  def distance_to(loc)
+    self.class.distance_to_location(loc,[self.lat,self.lon])
+  end
+
   private
   def self.distance_to_location(loc1,loc2)
     ## From http://stackoverflow.com/questions/12966638/how-to-calculate-the-
     ## distance-between-two-gps-coordinates-without-using-google-m
     rad_per_deg = Math::PI/180  # PI / 180
     rkm = 6371                  # Earth radius in kilometers
-    # rm = rkm * 1000             # Radius in meters
+    rm = rkm * 1000             # Radius in meters
 
     dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
     dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
@@ -28,6 +32,6 @@ class Location < ActiveRecord::Base
     a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad)* Math.sin(dlon_rad/2)**2
     c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
 
-    rkm * c # Delta in meters
+    rm * c # Delta in meters
   end
 end
--- lib/interpolator.rb
+++ lib/interpolator.rb
@@ -5,7 +5,25 @@ require 'debugger'
 include Math
 
 module Interpolator
+  # This is a local interpolation approach which uses a subset of locations
+  # within a certain radius of the desired location along with a weighting
+  # approach that uses the distance from the location to weight the importance
+  # of the reading
+  # See: http://en.wikipedia.org/wiki/Inverse_distance_weighting
+  def inverse_distance_weighting(loc,time,attribute)
+    @@MAX_DIST = 10000
+    @@P = 2 # Degree of weighting factor
+    # Find neighbours with
+    neighbours = Location.find_neighbours(loc,@@MAX_DIST)
+    u_num = 0
+    u_den = 0
+    neighbours.each do |neighbour|
+      weight = 1/(neighbour.distance_to(loc)**@@P)
+      u_num += weight*neigh.prediction(time: time).send(attribute) ## Something like this
+      u_den += weight
+    end
 
+  end
 end
 
 module Geocode
