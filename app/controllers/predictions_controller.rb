b91e5a9eeba832ff95d5d103e86ed6078d8b44a6
Eamon Taaffe <eamontaaffe@gmail.com>
2015-05-19 12:16:48 +1000

    stuff

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,8 +1,17 @@
+require 'time'
+
 class PredictionsController < ApplicationController
   def get_prediction_by_postcode
-    params[:post_code]
-    params[:period]
-    @out = {}
+    postcode = params[:post_code]
+    period = params[:period]
+    @out = {location_id: postcode, predictions: {}}
+    startTime = DateTime.now
+    endTime = startTime + period.minutes
+    (startTime..endTime).step(15*60).each do |i|
+      @out[:predictions][i] = {
+        time: Date
+      }
+    end
     respond_to do |format|
       format.html
       format.json { render json: JSON.pretty_generate(@out.as_json) }
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
   get 'weather/locations' => 'locations#get_locations', :defaults => { :format => 'json' }
-  get '/weather/predicition/:post_code/:period', => 'predictions#get_prediction_by_postcode', :defaults => { :format => 'json' }
+  get '/weather/predicition/:post_code/:period' => 'predictions#get_prediction_by_postcode', :defaults => { :format => 'json' }
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
