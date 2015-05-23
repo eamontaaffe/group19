825921a46fc41c0ada90c476209c481c4cca002e
dhhong <dhhong@student.unimelb.edu.au>
2015-05-24 02:28:35 +1000

    FIO staggered updates added, scraper bugfixes
    Enabled FIO readings every 40min to stay under 3000 (3 unique keys) api
    call limit. Fixed some bugs in the scraper; incorrect BOM wind direction
    readings, broken FIO importer.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,16 +2,49 @@ require 'rufus-scheduler'
 
 updater = Rufus::Scheduler.new
 
+# BOM update every 10 minutes
 updater.every '10m', :first => :now do
   Location.all.each do |loc|
     loc.new_BOMreading
   end
 end
 
-=begin
-updater.every('30m') do
-  Location.all.each do |loc|
+# Staggered updates for FIO, 40 min blocks
+group = (Location.all.size/6).ceil
+# 1st batch of updates
+  Location.all[0..group].each do |loc|
     loc.new_FIOreading
   end
 end
-=end
+# 2nd batch of updates
+updater.every '40m', :first_in => '10m' do
+  Location.all[(group+1)..(2*group)].each do |loc|
+    loc.new_FIOreading
+  end
+end
+# etc ...
+updater.every '40m', :first_in => '15m' do
+  Location.all[(2*group+1)..(3*group)].each do |loc|
+    loc.new_FIOreading
+  end
+end
+updater.every '40m', :first_in => '20m' do
+  Location.all[(3*group+1)..(4*group)].each do |loc|
+    loc.new_FIOreading
+  end
+end
+updater.every '40m', :first_in => '25m' do
+  Location.all[(4*group+1)..(5*group)].each do |loc|
+    loc.new_FIOreading
+  end
+end
+updater.every '40m', :first_in => '30m' do
+  Location.all[(5*group+1)..(6*group)].each do |loc|
+    loc.new_FIOreading
+  end
+end
+updater.every '40m', :first_in => '35m' do
+  Location.all[(6*group+1)..(Location.all.size)].each do |loc|
+    loc.new_FIOreading
+  end
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -70,7 +70,7 @@ module Scraper
     self.wetBulb = doc.css("td[headers*='delta-t'][headers*=#{obs_station}]").text
     self.pressure = doc.css("td[headers*='pressure'][headers*=#{obs_station}]").text
     # Convert dir to bearing
-    self.windDirection= doc.css("td[headers*='wind-dir'][headers*=#{obs_station}]").text
+    self.windDirection= doc.css("td[headers*='-wind-dir'][headers*=#{obs_station}]").text
     self.windSpeed= doc.css("td[headers*='wind-spd-kmh'][headers*=#{obs_station}]").text
     self.rainSince9am = doc.css("td[headers*='rainsince9am'][headers*=#{obs_station}]").text
     # Set source identifier
@@ -89,10 +89,9 @@ module Scraper
       key = API_KEY_3
     end
     # Get longitude and latitude data for API call
-    lat_long = "#{Station.find(self.station_id).lat},#{Station.find(self.station_id).long}"
+    lat_long = "#{Location.find(self.location_id).lat},#{Location.find(self.location_id).lon}"
     # Read in data as JSON
-    # Cycle API keys here based off scheduler flag?
-    forecast = JSON.parse(open("#{BASE_URL}/#{key}/#{lat_long},#{self.time}?units=ca").read)
+    forecast = JSON.parse(open("#{BASE_URL}/#{key}/#{lat_long},#{current.to_i}?units=ca").read)
     # Aggregate new data with existing data in StationReading object
     self.temp = forecast["currently"]["temperature"]
     self.dewPoint = forecast["currently"]["dewPoint"]
